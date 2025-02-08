2ae624d10d41a719185d6205713f9a27bf898aeb
lugiadev <natsurblx@gmail.com>
2025-02-09 00:47:29 +0700

    #1 Commit

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,9 @@ let domains: IDomain[] = []
 /**
  * Fetches available domains from server
  */
-export async function fetchDomains<Random extends boolean = false> ({ page = 1, getRandomDomain = false as Random }: { page?: number, getRandomDomain?: Random } | undefined = {}): Promise<Random extends true ? IDomain : IDomain[]> {
+export async function fetchDomains<Random extends boolean = false>({ page = 1, getRandomDomain = false as Random }: { page?: number, getRandomDomain?: Random } | undefined = {}): Promise<Random extends true ? IDomain : IDomain[]> {
   return await new Promise(async (resolve, reject) => {
-    const response = await request().get(`/domains?page=${page}`).catch(e => e.response ?? e)
+    const response = await request().get(`/domains?page=${page}`).catch(() => null)
     if (response.status === 200) {
       domains = response.data.map((domain: IDomain) => {
         domain = formatDates(domain)
@@ -45,42 +45,46 @@ export async function fetchDomains<Random extends boolean = false> ({ page = 1, 
  * const account = await createAccount()
  * console.log(account.email)
  */
-export async function createAccount (address?: string, password?: string): Promise<Account> {
+export async function createAccount(address?: string, password?: string): Promise<Account> {
   return await new Promise(async (resolve, reject) => {
-    await fetchDomains().catch(reject)
+    try {
+      await fetchDomains();
 
-    if (typeof address !== 'string') {
-      address = `${randomString(16)}@${domains[Math.floor(Math.random() * domains.length)].domain}`
-    }
-
-    if (address.split('@').length === 1) {
-      if (domains.some(domain => domain.domain === address)) {
-        address = `${randomString(16)}@${address}`
-      } else {
-        address += `@${domains[Math.floor(Math.random() * domains.length)].domain}`
+      if (typeof address !== 'string') {
+        address = `${randomString(16)}@${domains[Math.floor(Math.random() * domains.length)].domain}`;
       }
+
+      if (address.split('@').length === 1) {
+        if (domains.some(domain => domain.domain === address)) {
+          address = `${randomString(16)}@${address}`;
+        } else {
+          address += `@${domains[Math.floor(Math.random() * domains.length)].domain}`;
+        }
+      }
+
+      if (typeof password !== 'string') {
+        password = randomString(16);
+      }
+
+      const response = await request().post('/accounts', { address, password }).catch(() => null);
+
+      if (response && response.status === 201) {
+        const account = new Account(Object.assign(response.data, { password }), CONFIG);
+        await account.fetch().catch(() => { });
+        await account.mails.fetchAll().catch(() => { });
+        resolve(account);
+        return;
+      }
+
+      reject(getError(response ?? new Error('Unknown error')));
+    } catch (error) {
+      reject(error);
     }
-
-    if (typeof password !== 'string') {
-      password = randomString(16)
-    }
-
-    const response = await request().post('/accounts', { address, password }).catch(e => e.response ?? e)
-
-    if (response.status === 201) {
-      const account = new Account(Object.assign(response.data, { password }), CONFIG)
-      await account.fetch().catch(reject)
-      await account.mails.fetchAll().catch(reject)
-      resolve(account)
-      return
-    }
-
-    reject(getError(response))
-  })
+  });
 }
 
-export async function loginAccount (address: string, password: string): Promise<Account>
-export async function loginAccount (token: string): Promise<Account>
+export async function loginAccount(address: string, password: string): Promise<Account>
+export async function loginAccount(token: string): Promise<Account>
 
 /**
  * Logs into an existing account
@@ -91,7 +95,7 @@ export async function loginAccount (token: string): Promise<Account>
  * const account = await loginAccount("myEmail@domain.com", "mySuperSecretPassword")
  * console.log(account.email)
  */
-export async function loginAccount (...args: [string, string] | [string]): Promise<Account> {
+export async function loginAccount(...args: [string, string] | [string]): Promise<Account> {
   return await new Promise(async (resolve, reject) => {
     const [addressOrToken, password] = args
 
@@ -102,14 +106,14 @@ export async function loginAccount (...args: [string, string] | [string]): Promi
     }
 
     if (typeof password === 'string') {
-      const tokenResponse = await request().post('/token', { address: addressOrToken, password }).catch(e => e.response ?? e) as AxiosResponse<{ token?: string }>
+      const tokenResponse = await request().post('/token', { address: addressOrToken, password }).catch(() => null) as AxiosResponse<{ token?: string }>
 
       if (tokenResponse.status !== 200 || typeof tokenResponse.data.token !== 'string' || tokenResponse.data.token === '') {
         reject(getError(tokenResponse))
         return
       }
 
-      const response = await request().get('/me', { headers: { Authorization: `Bearer ${tokenResponse.data.token}` } }).catch(e => e.response ?? e)
+      const response = await request().get('/me', { headers: { Authorization: `Bearer ${tokenResponse.data.token}` } }).catch(() => null)
 
       if (response.status === 200) {
         const account = new Account(Object.assign(response.data, { token: tokenResponse.data.token, password }), CONFIG)
@@ -121,7 +125,7 @@ export async function loginAccount (...args: [string, string] | [string]): Promi
 
       reject(getError(response))
     } else {
-      const response = await request().get('/me', { headers: { Authorization: `Bearer ${addressOrToken}` } }).catch(e => e.response ?? e)
+      const response = await request().get('/me', { headers: { Authorization: `Bearer ${addressOrToken}` } }).catch(() => null)
 
       if (response.status === 200) {
         const account = new Account(Object.assign(response.data, { token: addressOrToken }), CONFIG)
@@ -136,7 +140,7 @@ export async function loginAccount (...args: [string, string] | [string]): Promi
   })
 }
 
-export function setConfig (config: IConfig): void {
+export function setConfig(config: IConfig): void {
   request(config.mailService, config.axiosOptions)
   Object.assign(CONFIG, config)
 }
